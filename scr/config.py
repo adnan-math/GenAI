d2516b1eb9d2ec1fb8cf8056a30bf260289d04e1
Muhammad Adnan Anwar <67798646+adnan-math@users.noreply.github.com>
2026-04-06 17:17:47 +0100

    Update config.py

--- scr/config.py
+++ scr/config.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 CONFIG = {
-    "pdf_path": "d:/GenAI/rag-project/data/adnan_cv.pdf",
-    #"pdf_path": "d:/GenAI/rag-project/data/sample.pdf",
+    # Use relative path (relative to app.py)
+    "pdf_path": Path("data/adnan_cv.pdf"),
     "chunk_size": 150,
     "overlap": 50
 }
